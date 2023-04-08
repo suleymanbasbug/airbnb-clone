d1ed13cd7da55dadd8c159b7cfa66ce711889d46
Suleyman <suleymanbasbug@hotmail.com>
2023-04-08 19:26:02 +0300

    feat:individual listing view

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import { Listing, Reservation } from "@prisma/client";
+import { Reservation } from "@prisma/client";
 
-import { SafeUser } from "@/app/types";
+import { SafeListing, SafeUser } from "@/app/types";
 import { useRouter } from "next/navigation";
 import useCountries from "@/app/hooks/useCountries";
 import { useCallback, useMemo } from "react";
@@ -12,7 +12,7 @@ import HeartButton from "../HeartButton";
 import Button from "../Button";
 
 interface ListingCardProps {
-  data: Listing;
+  data: SafeListing;
   reservation?: Reservation;
   onAction?: (id: string) => void;
   disabled?: boolean;
--- app/page.tsx
+++ app/page.tsx
@@ -32,7 +32,7 @@ export default async function Home() {
           gap-8
         "
         >
-          {listings.map((listing: any) => (
+          {listings.map((listing) => (
             <ListingCard
               key={listing.id}
               data={listing}
